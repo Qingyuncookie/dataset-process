08318e5fc9f5cb5572cec12ed0fd6dbdf285755d
Liqingyun <38065502+Qingyuncookie@users.noreply.github.com>
2019-07-31 09:42:42 +0800

    Create save_array_as_nii.py

---
+++ save_array_as_nii.py
@@ -0,0 +1,17 @@
+import SimpleITK as sitk
+
+def save_array_as_nifty_volume(data, filename, reference_name = None):
+    """
+    save a numpy array as nifty image
+    inputs:
+        data: a numpy array with shape [Depth, Height, Width]
+        filename: the ouput file name
+        reference_name: file name of the reference image of which affine and header are used
+    outputs: None
+    """
+    img = sitk.GetImageFromArray(data)
+    if(reference_name is not None):
+        img_ref = sitk.ReadImage(reference_name)
+        img.CopyInformation(img_ref)
+    sitk.WriteImage(img, filename)
+# filename: save_folder+"/{}.nii".format(filename)
